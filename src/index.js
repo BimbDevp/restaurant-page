3ef6c6d6faacc9be7a5c7cc3e07ee6ac3f755efd
BimbDevp <BimbDevp@users.noreply.github.com>
2026-07-09 15:26:04 +0700

    feat: add heero section & menu image

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-
-
-console.log("Restaurant Page");
+import sectionImage from "./image/hero_section.jpeg";
+import menuImage1 from "./image/Grilled_fish.jpeg";
+import menuImage2 from "./image/Fried_shrimp.jpeg";
+import menuImage3 from "./image/Boiled_clams.jpeg";
