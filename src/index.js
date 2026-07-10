22312558ecffe98dc0a3788c9db879abc6e5f458
BimbDevp <BimbDevp@users.noreply.github.com>
2026-07-10 13:35:22 +0700

    delete render file

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import sectionImage from "./image/hero_section.jpeg";
 import menuImage1 from "./image/Grilled_fish.jpeg";
 import menuImage2 from "./image/Fried_shrimp.jpeg";
 import menuImage3 from "./image/Boiled_clams.jpeg";
+
