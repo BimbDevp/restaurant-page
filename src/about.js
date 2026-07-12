1d382e68a89485baa85cb23c40fd88e95ef6c4ba
BimbDevp <BimbDevp@users.noreply.github.com>
2026-07-12 10:29:34 +0700

    feat: complete styling menu & about page

--- src/about.js
+++ src/about.js
@@ -3,20 +3,27 @@ import chefGrilling from "./image/Chef_grilling_seafood.jpeg";
 
 export default function createAboutPage() {
     const container = document.createElement("div");
+    container.classList.add("about-container");
 
     const div1 = document.createElement("div");
+    div1.classList.add("about-section");
     const card1 = document.createElement("img");
+    card1.classList.add("about-image");
     card1.src = modernRestaurant;
     const textContainer1 = document.createElement("div");
+    textContainer1.classList.add("about-text");
     const headline1 = document.createElement("h3");
     headline1.textContent = "Commitment to Raw Materials"
     const copy1 = document.createElement("p");
     copy1.textContent = "Every exceptional dish begins with an uncompromising selection of ingredients. We ensure that every vegetable, piece of meat and spice that enters the preparation area has met our strict quality standard.";
 
     const div2 = document.createElement("div");
+    div2.classList.add("about-section");
     const card2 = document.createElement("img");
+    card2.classList.add("about-image");
     card2.src = chefGrilling;
     const textContainer2 = document.createElement("div");
+    textContainer2.classList.add("about-text");
     const headline2 = document.createElement("h3");
     headline2.textContent = "Kitchen & Operational Philosophy"
     const copy2 = document.createElement("p");
--- src/menu.js
+++ src/menu.js
@@ -5,58 +5,80 @@ import grilledIndonesian from "./image/Grilled_Indonesian.jpeg";
 
 export default function createMenuPage() {
     const container = document.createElement("div");
+    container.classList.add("menu-container");
 
     const h1 = document.createElement("h1");
     h1.textContent = "Exploring Flavours";
     
     const cardContainer = document.createElement("div");
+    cardContainer.classList.add("menu-grid");
     
     const card1 = document.createElement("div");
+    card1.classList.add("menu-card");
     const image1 = document.createElement
     ("img");
     image1.src = boiledClams;
+    const containerText1 = document.createElement("div");
+    containerText1.classList.add("text-container"); 
     const title1 = document.createElement("h3");
     title1.textContent = "Fresh Boiled Clams";
     const price1 = document.createElement("p");
     price1.textContent = "Rp 38.000";
     const cta1 = document.createElement("button");
+    cta1.classList.add("order-btn");
     cta1.textContent = "Order Now";
 
     const card2 = document.createElement("div");
+    card2.classList.add("menu-card");
     const image2 = document.createElement("img");
     image2.src = goldenPrawns;
+    const containerText2 = document.createElement("div");
+    containerText2.classList.add("text-container"); 
     const title2 = document.createElement("h3");
     title2.textContent = "Golden Buttery Fried Prawns";
     const price2 = document.createElement("p");
     price2.textContent = "Rp 38.000";
     const cta2 = document.createElement("button");
+    cta2.classList.add("order-btn");
     cta2.textContent = "Order Now";
 
     const card3 = document.createElement("div");
+    card3.classList.add("menu-card");
     const image3 = document.createElement("img");
     image3.src = grilledIndonesian;
+    const containerText3 = document.createElement("div");
+    containerText3.classList.add("text-container"); 
     const title3 = document.createElement("h3");
     title3.textContent = "Whole Grilled Indonesian Snapper"
     const price3 = document.createElement("p");
     price3.textContent ="Rp 45.000"
     const cta3 = document.createElement("button");
+    cta3.classList.add("order-btn");
     cta3.textContent = "Order Now";
 
+    containerText1.appendChild(title1);
+    containerText1.appendChild(price1);
+    containerText1.appendChild(cta1);
+    
+    containerText2.appendChild(title2);
+    containerText2.appendChild(price2);
+    containerText2.appendChild(cta2);
+    
+    containerText3.appendChild(title3);
+    containerText3.appendChild(price3);
+    containerText3.appendChild(cta3);
+
+
+    
 
     card1.appendChild(image1);
-    card1.appendChild(title1);
-    card1.appendChild(price1);
-    card1.appendChild(cta1);
+    card1.appendChild(containerText1);
 
     card2.appendChild(image2);
-    card2.appendChild(title2);
-    card2.appendChild(price2);
-    card2.appendChild(cta2);
+    card2.appendChild(containerText2);
 
     card3.appendChild(image3);
-    card3.appendChild(title3);
-    card3.appendChild(price3);
-    card3.appendChild(cta3);
+    card3.appendChild(containerText3);
 
     cardContainer.appendChild(card1);
     cardContainer.appendChild(card2);
